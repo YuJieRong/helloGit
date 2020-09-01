9c04a3145294638d0002b707a5643484dd50deb9
ryj <ryjcxy@aliyun.com>
2020-09-01 22:52:52 +0800

    第一次提交

---
+++ src/main/java/com/ryj/git/HelloGit.java
@@ -0,0 +1,15 @@
+package com.ryj.git;
+
+/**
+ * ClassName:HelloGit
+ * Package:
+ * Description:
+ *
+ * @Date:2020/9/1 22:45
+ * @Author:ryj
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
